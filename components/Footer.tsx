a19456afda0535ec9324315b823876742f4eff2d
frontendDieferson <diefersonfrontenddeveloper@gmail.com>
2023-04-15 14:53:00 -0300

    project Portfólio

---
+++ components/Footer.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Footer() {
+    const year = new Date().getFullYear();
+
+    return (
+        <footer className="light-bg flex justify-space">
+            <span className="white">Copyright © {year} <Link href="/">Dieferson Soares</Link> - All rights reserved.</span>
+            <ul className="flex">
+                <li>
+                    <a href="https://www.facebook.com/dieferson.soares.14" target="_blank" rel="noreferrer">
+                        <Image src="/icons/facebook.svg" width={24} height={24} alt="facebook-icon" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/eudiefersonsoares" target="_blank" rel="noreferrer">
+                        <Image src="/icons/instagram.svg" width={24} height={24} alt="instagram-icon" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/diefersonsoares/" target="_blank" rel="noreferrer">
+                        <Image src="/icons/linkedin.svg" width={24} height={24} alt="linkedin-icon" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/frontendDieferson" target="_blank" rel="noreferrer">
+                        <Image src="/icons/github.svg" width={24} height={24} alt="github-icon" />
+                    </a>
+                </li>
+               
+            </ul>
+        </footer>
+    )
+}
